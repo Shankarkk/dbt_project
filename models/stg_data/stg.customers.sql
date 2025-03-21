61ee74c5c16d3deb9b2ccee4e14b608f0812819c
mailbox869115@gmail.com <mailbox869115@gmail.com>
2025-03-21 10:46:07 +0000

    commit and sync

--- models/stg_data/stg.customers.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-WITH tb1  as(
- select
-        id ,
-        first_name,
-        last_name
-     from {{source('datafeed_shared_schema','source_customers')}})
-     select * from tb1
